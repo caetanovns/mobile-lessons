3e5d7cd20124164d18edb086c03055a7106d086e
caetanovns <caetanov120@gmail.com>
2023-04-05 12:25:25 -0300

    Correção dos diretórios do teste. Adicionado teste e teste solução

--- test_avp1/teste_1/src/mocks/contact.js
+++ test_avp1/teste_1/src/mocks/contact.js
@@ -5,11 +5,26 @@ import pedro from "../../assets/photos/pedro.jpg";
 import richarlison from "../../assets/photos/richarlison.jpeg";
 import ronaldo from "../../assets/photos/ronaldo.jpeg";
 import vini from "../../assets/photos/vini.jpeg";
+import davi_luiz from "../../assets/photos/davi_luiz.jpg";
+import ronaldo_fenomeno from "../../assets/photos/ronaldo_fenomeno.jpg";
+import ronaldo_gaucho from "../../assets/photos/ronaldo_gaucho.jpg";
+
+
 
 const contacts = [
     {
+        "photo": pedro,
+        "name": "Pedro",
+        "phone": "+55 (88) 6754-1231"
+    },
+    {
         "photo": messi,
         "name": "Lionel Messi",
+        "phone": "+55 (88) 6754-1231"
+    },
+    {
+        "photo": ronaldo_fenomeno,
+        "name": "Ronaldo",
         "phone": "+55 (88) 6754-1231"
     },
     {
@@ -22,11 +37,7 @@ const contacts = [
         "name": "Neymar Jr.",
         "phone": "+55 (88) 6754-1231"
     },
-    {
-        "photo": pedro,
-        "name": "Pedro",
-        "phone": "+55 (88) 6754-1231"
-    },
+
     {
         "photo": richarlison,
         "name": "Richarlison",
@@ -41,7 +52,19 @@ const contacts = [
         "photo": vini,
         "name": "Vini Jr.",
         "phone": "+55 (88) 6754-1231"
+    },
+    {
+        "photo": davi_luiz,
+        "name": "Davi Luiz",
+        "phone": "+55 (88) 4714-1334"
+    },
+
+    {
+        "photo": ronaldo_gaucho,
+        "name": "Ronaldinho",
+        "phone": "+55 (88) 2724-1251"
     }
+
 ];
 
 export default contacts;
