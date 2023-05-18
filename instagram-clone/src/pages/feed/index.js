f271dbaf8a2c7d9f6cffeee37a7f22014f6c0d4b
jorge <jorge.luiz.g217@gmail.com>
2023-05-18 11:31:58 -0300

    Trocando o icon de savepost, adicionando os posts e o feed do usuário logado

--- instagram-clone/src/pages/feed/index.js
+++ instagram-clone/src/pages/feed/index.js
@@ -2,23 +2,34 @@ import { FlatList, View } from "react-native";
 import Stories from "./components/stories";
 import TopContainer from "./components/top-container";
 
-
-import post_mock from '../../../mocks/posts';
 import Post from "./components/post";
+import {useEffect, useState} from "react";
+import api from "../../services/api";
 
 
 export default function Feed() {
+
+    const [UserPosts, setUserPosts] = useState()
+
+    useEffect(()=> {
+        api.get("/users").then((data) => {
+            setUserPosts(data.data[0].feed)
+            }
+        )
+    }, []);
+
+
     return (
-        <View style={{ backgroundColor: 'white' }}>
+        <View style={{ backgroundColor: 'white', marginBottom: 18}}>
             <TopContainer />
             <FlatList
-                data={post_mock}
+                data={UserPosts}
                 ListHeaderComponent={
                     <>
                         <Stories />
                     </>
                 }
-                renderItem={({ item }) => <Post liked={item.liked} />}
+                renderItem={({ item }) => <Post liked={item.liked} username={item.username} location={item.location} comment={item.comment} photo={item.photo} profile_photo={item.profile_photo}/>}
             />
         </View>
     );
